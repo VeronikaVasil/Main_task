560a47b2f3fc0fc9e788e76188468d73b850bd86
Veronika <valeria14081981@yandex.ru>
2022-09-17 15:21:41 +0300

    Программа о создании нового массива из строк из имеющихся массивов

---
+++ Program/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string[] array = { "1234", "15", "-2", "comp" };
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+void SortArray(string[] Array)
+{
+    Console.WriteLine("Новый массив:");
+    string[] newArray = new string[Array.Length];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            newArray[i] = array[i];
+            Console.WriteLine(newArray[i]);
+        }
+    }
+}
+SortArray(array);
+Console.ReadLine();
